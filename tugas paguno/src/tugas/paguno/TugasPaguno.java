4db0591fb8e019f373bf07fb1a288a882799f8d7
AdityaPaguno <43056959+AdityaPaguno@users.noreply.github.com>
2018-09-19 20:03:20 +0700

    Add files via upload

---
+++ tugas paguno/src/tugas/paguno/TugasPaguno.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.paguno;
+
+/**
+ *
+ * @author Nuser
+ */
+public class TugasPaguno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int n = 5;
+    for (int i=0;i<n;i++){
+        for (int j=0;j<n;j++){
+            if (j==0 || j==(n-1) || i==0 || i==(n-1)){
+                System.out.print("#");
+            }
+            else{
+                System.out.print("*");
+            }
+        }
+        System.out.
+                println("");
+}
+    }
+    
+}
+
+
